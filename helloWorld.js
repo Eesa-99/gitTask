6c62604566af6100f587d498c80ed2ed1abbe0ca
eesa-99 <eesaseedat@gmail.com>
2023-06-15 10:24:28 +0200

    Date log Fixes #2 copleted

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,13 @@
 const userInput = prompt("Please enter some text:");
 console.log("You entered: " + userInput);
 console.log("You are doing a good job");
+// Create a new Date object
+var currentDate = new Date();
+
+// Get the date and time components
+var date = currentDate.toDateString();
+var time = currentDate.toLocaleTimeString();
+
+// Display the date and time
+console.log("Date: " + date);
+console.log("Time: " + time);
